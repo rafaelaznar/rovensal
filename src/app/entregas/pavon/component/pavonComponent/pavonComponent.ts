98d932d11dd705c7d0c21616fce254cbb6055056
Alejandro Pavon <alejandropm06@gmail.com>
2025-11-06 12:06:36 +0100

    El component tiene gets ahora :3

--- src/app/entregas/pavon/component/pavonComponent/pavonComponent.ts
+++ src/app/entregas/pavon/component/pavonComponent/pavonComponent.ts
@@ -1,10 +1,64 @@
 import { Component } from '@angular/core';
+import { PavonService } from '../pavonService/pavon-service';
+import { Character } from '../pavonModel/characterInterface';
+import { Boss } from '../pavonModel/bossInterface';
+import { Weapon } from '../pavonModel/weaponInterface';
+import { Domain } from '../pavonModel/domainInterface';
 
 @Component({
   selector: 'app-pavon',
   imports: [],
   templateUrl: './pavonComponent.html',
   styleUrl: './pavonComponent.css',
-  standalone: true
+  standalone: true,
 })
-export class PavonComponent {}
+export class PavonComponent {
+
+  chacacters: Character[] = [];
+  bosses: Boss[] = [];
+  weapons: Weapon[] = [];
+  elements: Element[] = [];
+  domains: Domain[] = [];
+
+  constructor(private pavonService: PavonService){ }
+
+  ngOnInit(){
+    this.getChars();
+    this.getWeapons();
+    this.getBosses();
+    this.getElements();
+    this.getDomains();
+  }
+
+  getChars(){
+    this.pavonService.getAllCharacters().subscribe((charactersData) => {
+      console.log(charactersData);
+      this.chacacters=charactersData
+    });
+  }
+  getWeapons(){
+    this.pavonService.getAllWeapons().subscribe((weaponsData) => {
+      console.log(weaponsData);
+      this.weapons=weaponsData
+    });
+  }
+  getBosses(){
+    this.pavonService.getAllBosses().subscribe((bossesData) => {
+      console.log(bossesData);
+      this.bosses=bossesData
+    });
+  }
+  getElements(){
+    this.pavonService.getAllElements().subscribe((elementsData) => {
+      console.log(elementsData);
+      this.elements=elementsData
+    });
+  }
+  getDomains(){
+    this.pavonService.getAllDomains().subscribe((domainsData) => {
+      console.log(domainsData);
+      this.domains=domainsData
+    });
+  }
+
+}

291e94e7c07a73a00c633d5f51dca0430a57c9bc
rond88 <r0nald19202006@gmail.com>
2025-11-07 11:39:54 +0100

    Add category list routed component with HTML and TypeScript files

---
+++ src/app/entregas/reyna/components/category-list-routed/category-list-routed.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CategoryService } from '../../service/category.service';
+
+@Component({
+  selector: 'app-category-list-routed',
+  imports: [],
+  templateUrl: './category-list-routed.html',
+  styleUrl: './category-list-routed.css',
+})
+export class CategoryListRouted {
+
+}

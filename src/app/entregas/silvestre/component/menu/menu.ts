6b5334cb9e373b734f2d91ebea74a75f7cb69248
Linztark <anasilvesttttre@gmail.com>
2025-11-09 11:42:17 +0100

    Vamos añadiendo cositas a mi proyecto sobre el dbd

---
+++ src/app/entregas/silvestre/component/menu/menu.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-menu',
+  imports: [],
+  templateUrl: './menu.html',
+  styleUrl: './menu.css',
+})
+export class Menu {
+
+  activeRoute: string = '';
+
+  constructor(private oRouter: Router) {
+    // obtener la ruta activa a partir del router
+    this.oRouter.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.activeRoute = event.url;
+      }
+    });
+  }
+
+
+}

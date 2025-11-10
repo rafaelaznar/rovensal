5199958966fdd83f770b7144769d3a7b5948d253
Lucia Castanera <castaneralucia@gmail.com>
2025-11-10 12:42:45 +0100

    proyecto terminado

--- src/app/entregas/castanyera/component/castanyera-menu-button/castanyera-menu-button.ts
+++ src/app/entregas/castanyera/component/castanyera-menu-button/castanyera-menu-button.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import { CastanyeraConfirmDialog } from '../castanyera-confirm-dialogue/castanyera-confirm-dialog';
 
 @Component({
   selector: 'app-castanyera-menu-button',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, MatDialogModule, MatButtonModule, CastanyeraConfirmDialog],
   templateUrl: './castanyera-menu-button.html',
   styleUrls: ['./castanyera-menu-button.css'],
 })
-export class CastanyeraMenuButton {}
+export class CastanyeraMenuButton {
+  private dialog = inject(MatDialog);
+  private router = inject(Router);
+
+  openConfirm() {
+    const ref = this.dialog.open(CastanyeraConfirmDialog, { width: '360px' });
+    ref.afterClosed().subscribe((result) => {
+      if (result === true) {
+        this.router.navigate(['/']);
+      }
+    });
+  }
+}
